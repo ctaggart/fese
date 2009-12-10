1418770fa33826a051d1526b4a73ff435359f48f
cameron.taggart <cameron.taggart@bbd02a74-473a-11de-89ec-3d9467717a5a>
2009-12-10 19:54:03 +0000

    port of Monobjc console application tutorial to F# for a new blog post at http://blog.ctaggart.com/

---
+++ trunk/HelloFoundation/HelloFoundation.fs
@@ -0,0 +1,18 @@
+﻿
+open Monobjc
+open Monobjc.Cocoa
+
+// Load any required framework
+ObjectiveCRuntime.LoadFramework("Cocoa");
+// Do the bridge initialization
+ObjectiveCRuntime.Initialize();
+
+let main() =
+  printfn "Hello from F#"
+  use pool = new NSAutoreleasePool();
+  printfn "Username: %A" (FoundationFramework.NSFullUserName())
+  printfn "Home Dir: %A"(FoundationFramework.NSHomeDirectory())
+  use dict = new NSDictionary(new NSString "/System/Library/Frameworks/AppKit.framework/Resources/version.plist")
+  printfn "AppKit Version: %A" (dict.[new NSString("CFBundleVersion")].CastTo<NSString>())
+
+main()

13399d17b79d525e4bcffff93e71c63d344fe90a
cameron.taggart <cameron.taggart@bbd02a74-473a-11de-89ec-3d9467717a5a>
2009-12-17 02:40:29 +0000

    Qt from F# for today's blog post

---
+++ trunk/blog/2009-12-16/QtHelloWorld/QtHelloWorld.fs
@@ -0,0 +1,14 @@
+﻿module QtHellWorld
+
+open com.trolltech.qt.gui
+
+let main() =
+  QApplication.initialize [||]
+  let btn = QPushButton "Hello World!"
+  btn.setWindowTitle "Hello World Title"
+  btn.resize(320,240)
+  btn.show()
+  QApplication.exec() |> ignore
+  ()
+
+main()

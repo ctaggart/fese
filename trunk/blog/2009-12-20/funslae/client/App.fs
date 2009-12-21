e99ede6216d93846ba91647149aa9c36442c46e0
cameron.taggart <cameron.taggart@bbd02a74-473a-11de-89ec-3d9467717a5a>
2009-12-21 02:30:23 +0000

    for new blog: Hello World for Bing Maps + Silverlight + F#

---
+++ trunk/blog/2009-12-20/funslae/client/App.fs
@@ -0,0 +1,31 @@
+﻿namespace Funslae
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+open Microsoft.Maps.MapControl
+open Microsoft.Maps.MapControl.Core
+
+type App() as app =
+  inherit Application()
+  do
+    app.Startup.Add(fun args ->
+      let grid = Grid()
+
+      let map = Map()
+      grid.Children.Add map
+      
+      map.CredentialsProvider <- {
+        new CredentialsProvider() with
+          member x.GetCredentials(action) =
+            action.Invoke(
+              let credentials = Credentials()
+              credentials.ApplicationId <- "AsQUVMODsS2NezEnDpl2DUlyGSoEa9EZTqL9lxcBTcKn2v53mfKJ-VOLufebS4Km"
+              credentials
+            )
+      }
+
+      app.RootVisual <- grid
+      ()
+    )

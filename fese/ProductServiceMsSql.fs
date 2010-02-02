f654012e09e2511713434a74f323a2d114527c1e
cameron.taggart <cameron.taggart@bbd02a74-473a-11de-89ec-3d9467717a5a>
2010-02-02 06:14:47 +0000

    for my "Microsoft's Extensible Storage Engine with F#" blog post

---
+++ fese/ProductServiceMsSql.fs
@@ -0,0 +1,33 @@
+﻿namespace AdventureWorks
+
+open AdventureWorks
+open MsSqlReader
+
+type ProductServiceMsSql(cn) =
+  let readAll() =
+    seq {
+      use cmd = createCommand cn "select ProductID, Name, ProductNumber, Color, StandardCost, ListPrice, Size, Weight, ProductCategoryID, ProductModelID, SellStartDate, SellEndDate, DiscontinuedDate, ThumbNailPhoto, ThumbnailPhotoFileName, rowguid, ModifiedDate from SalesLT.Product"
+      use r = cmd.ExecuteReader()
+      while r.Read() do
+        yield
+          {
+            ID = readInt32 r 0;
+            Name = readString r 1;
+            Number = readString r 2;
+            Color = readNullableString r 3;
+            StandardCost = readNullableDecimalAsSingle r 4;
+            ListPrice = readNullableDecimalAsSingle r 5;
+            Size = readNullableString r 6;
+            Weight = readNullableDecimalAsSingle r 7;
+            CategoryID = readNullableInt32 r 8;
+            ModelID = readNullableInt32 r 9;
+            SellStartDate = readDateTime r 10;
+            SellEndDate = readNullableDateTime r 11;
+            DiscontinuedDate = readNullableDateTime r 12;
+            ThumbNailPhoto = readNullableBytes r 13;
+            ThumbnailPhotoFileName = readNullableString r 14;
+            RowGuid = readGuid r 15;
+            ModifiedDate = readDateTime r 16;
+          }
+    }
+  member x.ReadAll = readAll
